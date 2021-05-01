dd33dd0a942c00de7051bbe57fe32bf36d15d3b9
Sami <msamitekce@gmail.com>
2021-05-01 05:12:08 +0300

    first commit

---
+++ src/main/java/com/samitekce/sokdrycker/SokdryckerApplication.java
@@ -0,0 +1,13 @@
+package com.samitekce.sokdrycker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SokdryckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SokdryckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samitekce/sokdrycker/SokdryckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samitekce.sokdrycker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SokdryckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
